7bc5298cf92e7b7b82f540b377b5c9b1d4c7f781
xibrox <batonchik2004@gmail.com>
2023-09-03 13:51:08 +0200

    second

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /*webpack: (config) => {
+    webpack: (config) => {
         config.externals.push({
             "utf-8-validate": "commonjs utf-8-validate",
             bufferutil: "commonjs bufferutil",
         });
 
         return config;
-    },*/
+    },
     images: {
         domains: [
             "uploadthing.com"
